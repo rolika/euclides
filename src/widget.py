b97dcd60ed3da68c61ceaf558d30eeb897b3b511
rolika@nostromo <weisz.roland@invitel.hu>
2017-07-16 12:26:43 +0200

    creating and moving polygons

--- src/widget.py
+++ src/widget.py
@@ -9,6 +9,7 @@ from tkinter import ttk
 WIDTH = 640
 HEIGHT = 480
 BG_COLOR = "black"
+FG_COLOR = "white"
 
 
 class GameField(tk.Canvas):
@@ -22,13 +23,31 @@ class GameField(tk.Canvas):
         self["bg"] = BG_COLOR
         self.grid()
 
+    def add_poly(self, polygon):
+        """ Add a polygon to the canvas and set its tkinter id.
+            polygon: a Polygon-object
+        """
+        polygon.tkid = self.create_polygon(
+            polygon.coords(), fill="", outline=FG_COLOR)
+
+    def move_poly(self, polygon, x, y):
+        """ Relocate polygon on the canvas.
+            polygon: a Polygon-object
+            x: new horizontal coordinate of polygons center point
+            y: new vertical coordinate of polygons center point
+        """
+        polygon.move(x, y)
+        self.coords(polygon.tkid, *polygon.coords())
+
 
 def test():
     """ Testing this class """
     import polygon
-    p = polygon.Polygon(50, 9)
     g = GameField()
-    p.create(g)
+    p = polygon.Polygon(50, 7)
+    p.move(320, 200)
+    g.add_poly(p)
+    g.bind("<Motion>", lambda event: g.move_poly(p, event.x, event.y))
     g.mainloop()
 
 
