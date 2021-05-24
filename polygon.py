20e4d1535434392839f871a565d1ace388dc4904
Weisz Roland <weisz.roland@wevik.hu>
2021-05-24 09:22:37 +0200

    polygon is a sprite

--- polygon.py
+++ polygon.py
@@ -1,18 +1,24 @@
 import pygame
+from pygame import sprite
 from pygame.locals import *
 import math
 
 
-class Polygon(pygame.Surface):
+class Polygon(sprite.Sprite):
     """Draw and manage a regular polygon."""
-    def __init__(self, size, n=3):
+    def __init__(self, size, n=3) -> None:
         """Prepare a surface containing the polygon.
         size:   size of containing surface (rectangular area as the polygon is regular)
-        n:      this will be an n-sided polygon"""
-        super().__init__((size, size))
+        n:      this will be an n-sided polygon (also represents the hull-thickness)"""
+        super().__init__()
         self._size = size
         self._n = n
-        pygame.draw.polygon(self, pygame.Color("white"), self._calculate_vertices(), n)
+        self.image = pygame.Surface((size, size))
+        pygame.draw.polygon(self.image, pygame.Color("white"), self._calculate_vertices(), n)
+        self.rect = self.image.get_rect()
+    
+    def update(self):
+        self.rect.center = 400, 300
     
     def _calculate_vertices(self):
         """Calculate the vertices of the polygon.
@@ -31,14 +37,16 @@ def test_polygon() -> None:
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Test Polygon")
 
-    poly = Polygon(120, 3)
+    poly = Polygon(160, 7)
+    sprites = sprite.RenderPlain((poly, ))
 
     while 1:
         screen.fill((0,0,0))
         for event in pygame.event.get():
             if event.type == QUIT:  # exit by closing the window
                 sys.exit()
-        screen.blit(poly, (400, 300))
+        sprites.update()
+        sprites.draw(screen)
         pygame.display.update()
     
 
