5a28362e51c9e0062ebbc24991492a075fb6a143
Weisz Roland <weisz.roland@wevik.hu>
2021-05-24 14:16:41 +0200

    main application class

--- polygon.py
+++ polygon.py
@@ -2,8 +2,10 @@ import pygame
 from pygame import sprite
 from pygame.locals import *
 import math
+import sys
 
 
+SCREEN_SIZE = (800, 600)
 PLAYER_SIZE = 60
 PLAYER_VERTICES = 3  # a triangle
 
@@ -15,53 +17,52 @@ class Polygon(sprite.Sprite):
         size:   size of containing surface (rectangular area as the polygon is regular)
         n:      this will be an n-sided polygon"""
         super().__init__()
-        self._size = size
-        self._n = n
         self.image = pygame.Surface((size, size))
-        pygame.draw.polygon(self.image, pygame.Color("white"), self._calculate_vertices(), n)
+        pygame.draw.polygon(self.image, pygame.Color("white"), self._vertices(size, n), n)
         self.rect = self.image.get_rect()
-    
-    def _calculate_vertices(self):
+
+    def _vertices(self, size, n):
         """Calculate the vertices of the polygon.
         Returns:    a list of pair of coordinates"""
-        r = self._size // 2  # radius of enclosing circle
-        angle = math.radians(360 / self._n)  # inner angle of polygon
+        r = size // 2  # radius of enclosing circle
+        angle = math.radians(360 / n)  # inner angle of polygon
         # 'r +' means here that origin is in the rect's middle
-        return [[int(r + r*math.sin(i*angle)), int(r + r*math.cos(i*angle))] for i in range(0, self._n)]
+        return [[int(r + r*math.sin(i*angle)), int(r + r*math.cos(i*angle))] for i in range(0, n)]
 
 
 class Player(Polygon):
-    """Player is a type of polygon."""
+    """Player is a triangle."""
     def __init__(self):
-        """Init a triangle, with an angle upwards, representing a simple starship."""
+        """Initialize a triangle, representing a starfighter."""
         super().__init__(PLAYER_SIZE, PLAYER_VERTICES)
-        self.image = pygame.transform.rotate(self.image, 180)  # turn upside down so it has an angle upward
-    
+        self.image = pygame.transform.rotate(self.image, 180)  # turn upside down so it has tip upward
+
     def update(self):
         self.rect.center = pygame.mouse.get_pos()  # bind the movement to the mouse pointer
 
 
-def test_polygon() -> None:
-    """Testing the Polygon class"""
-    import sys
+class Euclides:
+    """Main game application."""
+    def __init__(self) -> None:
+        pygame.init()
+        self._screen = pygame.display.set_mode(SCREEN_SIZE)
+        pygame.display.set_caption("Euclides")
+        self._main()
 
-    pygame.init()
-    screen = pygame.display.set_mode((800, 600))
-    pygame.display.set_caption("Test Polygon")
+    def _main(self) -> None:
+        """Execute the application."""
+        player = Player()
+        sprites = sprite.RenderPlain((player, ))
 
-    player = Player()
-    sprites = sprite.RenderPlain((player, ))
-
-    while 1:
-        screen.fill((0,0,0))
-        for event in pygame.event.get():
-            if event.type == QUIT:  # exit by closing the window
-                sys.exit()
-        sprites.update()
-        sprites.draw(screen)
-        pygame.display.update()
-    
+        while 1:
+            self._screen.fill((0, 0, 0))
+            for event in pygame.event.get():
+                if event.type == QUIT:  # exit by closing the window
+                    sys.exit()
+            sprites.update()
+            sprites.draw(self._screen)
+            pygame.display.update()
 
 
 if __name__ == "__main__":
-    test_polygon()
+    Euclides()
